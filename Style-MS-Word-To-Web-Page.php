bc1e81520f17def45e1dd96cf64428f75f163108
TARA <83545951+Tara-Sadega@users.noreply.github.com>
2021-05-02 22:42:09 +0700

    Update Style-MS-Word-To-Web-Page.php

--- Style-MS-Word-To-Web-Page.php
+++ Style-MS-Word-To-Web-Page.php
@@ -1,3 +1,11 @@
+<head>
+  <meta name="hostname" content="Opposite1996">
+  <meta name="user-login" content="Customs Name">
+  <link rel="fluid-icon" href="https://github.com/fluidicon.png" title="GitHub">
+  <link rel="mask-icon" href="https://github.githubassets.com/pinned-octocat.svg" color="#000000">\
+  <link rel="alternate icon" class="js-site-favicon" type="image/png" href="https://github.githubassets.com/favicons/favicon.png">
+  <link rel="icon" class="js-site-favicon" type="image/svg+xml" href="https://github.githubassets.com/favicons/favicon.svg">
+</head>
 <?php
   echo "Hello World";
 ?>
