b8f5f7ed817e70417653e39defed0431390ec901
TARA <83545951+Tara-Sadega@users.noreply.github.com>
2021-05-02 23:01:32 +0700

    Update Style-MS-Word-To-Web-Page.php

--- Style-MS-Word-To-Web-Page.php
+++ Style-MS-Word-To-Web-Page.php
@@ -1,14 +1,21 @@
-<head>
-  <meta name="hostname" content="Opposite1996">
-  <meta name="user-login" content="Customs Name">
-  <link rel="fluid-icon" href="https://github.com/fluidicon.png" title="GitHub">
-  <link rel="mask-icon" href="https://github.githubassets.com/pinned-octocat.svg" color="#000000">\
-  <link rel="alternate icon" class="js-site-favicon" type="image/png" href="https://github.githubassets.com/favicons/favicon.png">
-  <link rel="icon" class="js-site-favicon" type="image/svg+xml" href="https://github.githubassets.com/favicons/favicon.svg">
-</head>
 <?php
-  echo "Hello World";
+  $T_Info = [
+    'Title'     => 'MS Word To Web Page',
+    'Author'    => 'TARA SHADEGA',
+    'Developer' => 'NSCSoft',
+    'Hostname'  => 'NSCSoft.inc'
+	];
+
+/*/
+/ <?php echo $T_Info['']; ?>
+/*/
 ?>
+<meta name="hostname" content="<?php echo $T_Info['Hostname']; ?>">
+<meta name="user-login" content="Customs Name">
+<link rel="fluid-icon" href="https://github.com/fluidicon.png" title="<?php echo $T_Info['Title']; ?>">
+<link rel="mask-icon" href="https://github.githubassets.com/pinned-octocat.svg" color="#000000">\
+<link rel="alternate icon" class="js-site-favicon" type="image/png" href="https://github.githubassets.com/favicons/favicon.png">
+<link rel="icon" class="js-site-favicon" type="image/svg+xml" href="https://github.githubassets.com/favicons/favicon.svg">
 <style>
   *{
     Margin:0px;
